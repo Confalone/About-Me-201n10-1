1eb1a83aa03a1bc67196c9ad8c796b9fda57b942
Richard Tae <taerichard@yahoo.com>
2018-01-11 23:36:54 -0800

    updated lab 2

--- app.js
+++ app.js
@@ -9,19 +9,21 @@ var user = prompt('What is your Name?');
 console.log('User\'s name: ' + user);
 alert('Hi ' + user + '! I am so glad you want to play Richard\'s game.');
 
-var whichAsian = prompt('Is Richard\'s nationality Chinese? type yes or no').toLowerCase(); //check this out .becomes lowercase.
-console.log(user + ' answered Question 1 ' + whichAsian);
+var richardsNationality = prompt('Is Richard\'s nationality Chinese? type yes/no or y/n').toLowerCase();
+console.log(user + ' answered Question 1 ' + richardsNationality);
 
-if(whichAsian === 'yes' || whichAsian === 'y'){
+if(richardsNationality === 'yes' || richardsNationality === 'y'){
     alert('You are incorrect! Richard is Korean!');
-} else {   
+} 
+else {   
     alert('You are correct! Richard is Korean!');
     userPoints++;
 }
 
-var richardsHometown = prompt(user + ', Is Richard born in Los Angeles? Please type yes or no.').toLowerCase();
+var richardsHometown = prompt(user + ', Is Richard born in Los Angeles? Please type yes/no or y/n.').toLowerCase();
 console.log(user + ' answered ' + richardsHometown);
-if(richardsHometown === 'yes' || richardsHometown === 'Yes') {
+
+if(richardsHometown === 'yes' || richardsHometown === 'y') {
     alert('Great job ' +  user + ',  Richard is born in Los Angeles');
     userPoints++;
 }
@@ -29,9 +31,10 @@ else {
     alert('Richard is from Los Angeles!');
 }
 
-var richardsHobby = prompt('Is Richard\'s favorite hobby playing basketball? Please type yes or no.').toLowerCase();
+var richardsHobby = prompt('Is Richard\'s favorite hobby playing basketball? Please type yes/no or y/n.').toLowerCase();
 console.log(user + ' answered question 3 ' + richardsHobby);
-if(richardsHobby === 'yes' || richardsHobby === 'Yes') {
+
+if(richardsHobby === 'yes' || richardsHobby === 'y') {
     alert('Richard enjoys playing basketball');
     userPoints++;
 }
@@ -39,10 +42,10 @@ else {
     alert('Nice try ' + user + '!');
 }
 
-
-var richardsFood = prompt('Is Richard\'s favorite food pizza?').toLowerCase();
+var richardsFood = prompt('Is Richard\'s favorite food pizza? Please type yes/no or y/n').toLowerCase();
 console.log(user + ' answered question 4 ' + richardsFood);
-if(richardsFood === 'yes') {
+
+if(richardsFood === 'yes' || richardsHobby === 'y') {
     alert('Nice job ' + user +', Richard\'s favorite food is most def, PIZZA!');
     userPoints++;
 }
@@ -50,37 +53,20 @@ else {
     alert(user + ', nice try, better luck next time!');
 }
 
-var richardsColor = prompt('Is Richard\'s favorite color red?').toLowerCase();
+var richardsColor = prompt('Is Richard\'s favorite color red? Please type yes/no or y/n').toLowerCase();
 console.log(user + ', answered question 5 ' + richardsColor);
-if(richardsColor === 'yes') {
+
+if(richardsColor === 'yes' || richardsColor === 'y') {
     alert('Richard\'s favorite color is red!')
     userPoints++;
-   }
-
+} 
 else {
-    alert('Nice try ' + user + 'hope you have better luck next time!')
-}
-    alert(user + ' got ' + userPoints + ' questions correct!  Nicely done ' + user);
-
-
-
-//check if the user's answer is correct 
-//what is correct? 
-//find out what the answer is
-//toLowerCase or toUpperCase
-//use a confirm box
-//
-/*
-if(answer === 'yes' || answer === 'y');{
-    //if it's correct then give them a point
-    userPoints++;
-    // if answer is yes, javascript will read the lowercase and will get a point
-} else {
-    //if it's not correct, tell them to tryt again. 
-    alert('Nope! I love all spicey food');
-
+    alert('Nice try ' + user + ' hope you have better luck next time!')
 }
 
-
-alert('You have ' + userPoints + ' points');
-*/
+if(userPoints <= 2){
+    alert(user + ', you got ' + userPoints + ' questions correct. Try harder next time');
+}
+else{
+    alert(user + ', you got ' + userPoints + ' questions correct. Nicely done!');
+}
